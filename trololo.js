fd2b5187e3b66c1b95a6b3aaba97747f4c112ca2
Evgeniy Vasilev <Evgeniy.Vasilev@netent.com>
2017-01-18 08:22:25 +0100

    More working version

--- trololo.js
+++ trololo.js
@@ -1,33 +1,23 @@
 var ids = [320465];
 (function($){
 
-	function create_overlay($, $el) {
-		var d = document.createElement('div');
-		$(d).css({
-			background: 'black',
-			opacity: '0.5',
-			width: '100%',
-			height: '100%',
-			position: 'absolute',
-			top: 0,
-			left: 0,
-			display: 'none',
-			zIndex: 100
-		}).attr('class', 'trololo-overlay');
-		$el.prepend(d);
-	}
-
 	function run() {
-		$('.comment').each(function (i, commentTable){
+		$(document).on('click', function(event) {
+			$target = $(event.target)
+			if ($target.hasClass('trololo-overlay')) {
+				$($target.children(0)).unwrap()
+			}
+		});
+		$('table.comment').each(function (i, commentTable){
 			$commentTable = $(commentTable);
+			console.log($commentTable);
 			var profileId = parseInt($($commentTable.find('a.lgshowprofile').get(0)).data('href').split('uid=')[1], 10);
 			if (profileId) {
-				$commentTable.find('.cmntTxt').css('opacity', '0.05')
-				// create_overlay($, $commentTable)
+				$overlay = $('<div class="trololo-overlay"></div>');
+				$commentTable.wrap($overlay)
 			}
 			console.log($commentTable, profileId);
 		});
-		// $($([0])
 	}
 
 	run();
